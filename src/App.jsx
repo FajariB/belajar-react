82528fdfa2ae512662b48da0a49e1defeb797e23
fajari bagas <fajaribagas09@gmail.com>
2025-03-18 00:33:16 +0700

    belajar sampe lifting state up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-import './App.css'
-import { Login } from './pages/Login/login'
+import { useState } from 'react';
 
-function App() {
+function Square({ value, onSquareClick }) {
   return (
-    <Login />
-  )
+    <button className='square' onClick={onSquareClick}>
+      {value}
+    </button>
+  );  
 }
 
-export default App
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext); //belom mau ke trigger ketika pencet square nya, perlu dicek lagi. sampe lifting state up
+  }
+  return (
+    <div className='board'> 
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+      <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+      <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+      <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+      <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+      <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+      <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+    </div>
+  );
+};
